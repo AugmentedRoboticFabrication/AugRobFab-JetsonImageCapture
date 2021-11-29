a6df61b45ad1ad6c90dbf733f8904caefc573d63
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2021-11-29 14:57:20 -0500

    Update testKBI.py

--- testKBI.py
+++ testKBI.py
@@ -9,29 +9,28 @@ try:
 	pin15 = GPIO.input(15)
 	pin16 = GPIO.input(16)
 
-	try:
-		while True:
-			curPin15 = GPIO.input(15)
-			curPin16 = GPIO.input(16)
+	while True:
+		curPin15 = GPIO.input(15)
+		curPin16 = GPIO.input(16)
 
-			print("Pin 15: %d" % curPin15)
-			print("Pin 16: %d" % curPin16)
+		print("Pin 15: %d" % curPin15)
+		print("Pin 16: %d" % curPin16)
 
-			# if pin15 1->0 | DO 0->1 (start recording)
-			if pin15 and not curPin15:
-				print('start recording')
-			# if pin15 0->1 | DO 1->0 (end recording)
-			if not pin15 and curPin15:
-				print('end recording')
-			# if pin15=0 | DO 1 && pin16 1->0 (capture frame)
-			if not curPin15 and (pin16 and not curPin16):
-				print('record frame')
-			pin15 = curPin15
-			pin16 = curPin16
+		# if pin15 1->0 | DO 0->1 (start recording)
+		if pin15 and not curPin15:
+			print('start recording')
+		# if pin15 0->1 | DO 1->0 (end recording)
+		if not pin15 and curPin15:
+			print('end recording')
+		# if pin15=0 | DO 1 && pin16 1->0 (capture frame)
+		if not curPin15 and (pin16 and not curPin16):
+			print('record frame')
+		pin15 = curPin15
+		pin16 = curPin16
 
-			time.sleep(.1)
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-		print('\nForced quit.')
-	except:
-		GPIO.cleanup()
+		time.sleep(.1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print('\nForced quit.')
+except:
+	GPIO.cleanup()
