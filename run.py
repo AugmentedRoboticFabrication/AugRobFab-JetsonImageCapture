36c80366b76f76cb6a63b9ba4e3b932d02517ead
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2021-11-29 18:17:34 -0500

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 	GPIO.setup(16, GPIO.IN)
 	
 	try:
-		print(config.gui)
+		isRecording = False
 		recorder = Recorder(config.gui, config.rec_config)
 
 		pin15 = GPIO.input(15)
@@ -30,18 +30,19 @@ if __name__ == '__main__':
 			curPin15 = GPIO.input(15)
 			curPin16 = GPIO.input(16)
 
-			# if pin15 1->0 | DO 0->1 (start recording)
-			if pin15 and not curPin15:
-				fn = config.fn + '_%d' % round(time.time())
-				recorder.start(fn)
+			if isRecording:
+				# if pin15 0->1 | DO 1->0 (end recording)
+				if not pin15 and curPin15:
+					recorder.end()
 
-			# if pin15 0->1 | DO 1->0 (end recording)
-			if not pin15 and curPin15:
-				recorder.end()
-
-			# if pin15=0 | DO 1 && pin16 1->0 (capture frame)
-			if not curPin15 and (pin16 and not curPin16):
-				recorder.recordFrame()
+				# if pin15=0 | DO 1 && pin16 1->0 (capture frame)
+				if not curPin15 and (pin16 and not curPin16):
+					recorder.recordFrame()
+			else:
+				# if pin15 1->0 | DO 0->1 (start recording)
+				if pin15 and not curPin15:
+					fn = config.fn + '_%d' % round(time.time())
+					recorder.start(fn)
 
 			pin15 = curPin15
 			pin16 = curPin16
