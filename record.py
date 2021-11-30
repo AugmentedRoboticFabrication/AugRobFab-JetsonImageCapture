44388bd034f3483c52be3f5ec89a2c224ba8e334
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2021-11-29 21:59:01 -0500

    Update record.py

--- record.py
+++ record.py
@@ -1,10 +1,10 @@
-import os, time
+import os, time, shutil
 from configargparse import ArgParser
 import RPi.GPIO as GPIO
 import open3d as o3d
 
 class azureKinectMKVRecorder:
-	def __init__(self, fn, gui, rec_config, out_dir):
+	def __init__(self, fn, gui, rec_config):
 		# GPIO config
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(15, GPIO.IN)
@@ -23,13 +23,21 @@ class azureKinectMKVRecorder:
 		#OS variables
 		self.fn = fn
 		self.dir = os.getcwd()
-		self.abspath = "{}/{}".format(self.dir,self.fn)
-
-		if not os.path.exists(self.abspath):
-			os.mkdir(self.abspath)
+		self.abspath = "%s/%s" % (self.dir,self.fn)
+		
+		self.out_dir = None
+		mediaList = os.listdir('/media/nano'))
+		mediaList.remove('L4T-README')
+		if len(mediaList) == 0:
+			print('No External USB storage device found. Falling back to CWD.')
+		elif len(mediaList) == 1:
+			self.out_dir = '/media/nano/%s' % mediaList[0]
+			print('Found External USB storage device for backup: %s.' % mediaList[0])
+		else:
+			print('Multiple External USB storage devices found. Falling back to CWD.')
 		
 		#Azure Config
-		self.rec_config = o3d.io.read_azure_kinect_sensor_config('{}/{}'.format(self.dir, rec_config))
+		self.rec_config = o3d.io.read_azure_kinect_sensor_config('%s/%s' % (self.dir, rec_config))
 		
 		self.recorder = o3d.io.AzureKinectRecorder(self.rec_config, 0)
 		if not self.recorder.init_sensor():
@@ -40,9 +48,9 @@ class azureKinectMKVRecorder:
 	def start(self):
 			fn = self.fn + '_%d' % round(time.time())
 			if not self.recorder.is_record_created():
-				if not os.path.exists('{}/{}'.format(self.dir,fn)):
-					os.mkdir('{}/{}'.format(self.dir,fn))
-				self.recorder.open_record("{}/{}/capture.mkv".format(self.dir,fn))
+				if not os.path.exists('%s/%s' % (self.dir,fn)):
+					os.mkdir('%s/%s' % (self.dir,fn))
+				self.recorder.open_record('%s/%s/capture.mkv' % (self.dir,fn))
 			print('Starting Recording: %s' % fn)
 	def end(self):
 		if self.recorder.is_record_created():
@@ -51,6 +59,13 @@ class azureKinectMKVRecorder:
 			self.vis.clear_geometries()
 			self.vis.close()
 		self.counter = 0
+
+		# Copy capture to external USB storage device
+		if self.out_dir is not None:
+			print("Backing up capture to %s..."% self.out_dir, end="")
+			shutil.copytree('%s/%s' % (self.dir,fn), self.out_dir) 
+			print("Done!")
+
 		return False
 
 	def frame(self):
@@ -64,11 +79,11 @@ class azureKinectMKVRecorder:
 
 			self.vis.update_geometry(rgbd)
 			self.vis.update_renderer()
+		self.counter+=1
 
 	def exit(self, vis):
 		self.isRunning = False
 
-		self.counter+=1
 		return False
 
 ##############################################################################
@@ -114,9 +129,11 @@ class azureKinectMKVRecorder:
 				time.sleep(.1)
 			GPIO.cleanup()
 			print('Bye!')
+
 		except KeyboardInterrupt:
 			GPIO.cleanup()
 			print('\nBye!')
+
 		except Exception as e:
 			GPIO.cleanup()
 			print(e)
@@ -126,10 +143,9 @@ if __name__ == '__main__':
 	parser.add('--fn', default='capture')
 	parser.add('--gui', action='store_true')
 	parser.add('--rec_config', help='relative path to rec_config.json file.', default='rec_config.json')
-	parser.add('--out_dir', default=None)
 
 	config = parser.parse_args()
 
-	recorder = azureKinectMKVRecorder(config.fn, config.gui, config.rec_config, config.out_dir)
+	recorder = azureKinectMKVRecorder(config.fn, config.gui, config.rec_config)
 	recorder.run()
 
